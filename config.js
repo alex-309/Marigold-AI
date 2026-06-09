1e049fb6581c31b85211585dd90762131ef7946d
alex-309 <alexjua3009@gmail.com>
2026-06-08 22:17:35 -0500

    update API_BASE to Render URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Change this to your deployed backend URL before uploading to S3.
 // Example: const API_BASE = "https://marigold-api.onrender.com";
-const API_BASE = "http://localhost:5001";
+const API_BASE = "https://marigold-ai.onrender.com";
